37514fa496fa762485d254b81d3e00fe2d4a5dd3
xavierwoo <xavier.woo@gmail.com>
2025-06-15 11:17:50 +0800

    字符串括号匹配

---
+++ algorithm.cpp
@@ -0,0 +1,31 @@
+#include "algorithm.h"
+#include <stack>
+
+auto hbutds::check_brackets_matching(const std::string& str) -> bool{
+    std::stack<char> stk;
+    for(char c : str){
+        switch (c) {
+            case '(':
+            case '[':
+            case '{':
+                stk.push(c);
+                break;
+            case ')':
+                if (stk.empty()) return false;
+                if (stk.top() != '(') return false;
+                stk.pop();
+                break;
+            case ']':
+                if (stk.empty()) return false;
+                if (stk.top() != '[') return false;
+                stk.pop();
+                break;
+            case '}':
+                if (stk.empty()) return false;
+                if (stk.top() != '{') return false;
+                stk.pop();
+                break;
+        }
+    }
+    return stk.empty();
+}
---
+++ algorithm.h
@@ -0,0 +1,10 @@
+#ifndef HBUTDS_ALGORITHM_H
+#define HBUTDS_ALGORITHM_H
+
+#include <string>
+
+namespace hbutds{
+    auto check_brackets_matching(const std::string&) -> bool;
+}
+
+#endif
