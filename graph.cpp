b2fad80475daa870f4eef95f20c68b6327c52730
xavierwoo <xavier.woo@gmail.com>
2025-08-04 10:41:40 +0800

    Graph类基础函数

---
+++ graph.cpp
@@ -0,0 +1,32 @@
+#include "graph.h"
+#include <iostream>
+using std::cout;
+namespace hbutds{
+
+    void test_add_vertex(){
+        Graph<char> graph;
+        graph.add_vertex('a');
+        graph.add_vertex('b');
+        assert(graph.get_vertex_id('a').value() == 0);
+        assert(graph.get_vertex_id('b').value() == 1);
+
+        cout<<"@ graph::add_vertex works\n";
+    }
+
+    void test_add_edge(){
+        Graph<char> graph;
+        graph.add_vertex('a');
+        graph.add_vertex('b');
+        graph.add_edge('a', 'b', 2);
+        assert(graph.get_edge_cost('a', 'b') == 2);
+        assert(graph.get_edge_cost('b', 'a') 
+            == std::numeric_limits<double>::infinity());
+        
+        cout<<"@ graph::add_edge works\n";
+    }
+    void graph_works(){
+        test_add_vertex();
+        test_add_edge();
+        cout<<"hbutds::Graph works!\n";
+    }
+}
---
+++ graph.h
@@ -0,0 +1,111 @@
+#ifndef HBUTDS_GRAPH_H
+#define HBUTDS_GRAPH_H
+
+#include "vector.h"
+#include <optional>
+#include <cassert>
+#include <limits>
+
+namespace hbutds{
+    
+    template <typename T>
+    struct Graph{
+    private:
+        struct ToEdge;
+        vector<std::optional<T>> _vertices; // 顶点表
+        vector<vector<ToEdge>> _adjacency_list; //邻接表
+
+        unsigned int _vertex_size{0}; //顶点个数
+        unsigned int _edge_size{0}; // 边个数
+
+        
+    public:
+
+        auto vertex_size() {return _vertex_size;}
+        auto edge_size() {return _edge_size;}
+
+        //获取顶点编号
+        auto get_vertex_id(const T&) const -> std::optional<unsigned int>;
+
+        //添加顶点
+        auto add_vertex(const T&) -> unsigned int;
+
+        //添加变
+        void add_edge(const T&, const T&, const double);
+
+        //获取边权值
+        auto get_edge_cost(const T&, const T&) const -> double;
+
+        //使用顶点id获得边权值
+        auto get_edge_cost_by_id(
+                const unsigned int, const unsigned int) const -> double;
+    }; 
+
+    template <typename T>
+    struct Graph<T>::ToEdge{
+        unsigned int to;
+        double cost;
+    };
+
+    void graph_works();
+}
+
+
+template <typename T>
+auto hbutds::Graph<T>::get_vertex_id(
+        const T& vertex
+) const -> std::optional<unsigned int>{
+    for(auto i=0; i<_vertices.size(); ++i){
+        if (_vertices[i].has_value() && _vertices[i].value() == vertex){
+            return std::optional(i);
+        }
+    }
+    return std::nullopt;
+}
+
+template <typename T>
+auto hbutds::Graph<T>::add_vertex(const T& vertex) -> unsigned int{
+    assert(! get_vertex_id(vertex).has_value());
+    auto id{_vertices.size()};
+    _vertices.push_back(vertex);
+    _adjacency_list.push_back({});
+    return id;
+}
+
+
+template <typename T>
+void hbutds::Graph<T>::add_edge(
+    const T& source, const T& target, const double cost
+){
+    auto s_id {get_vertex_id(source)};
+    auto t_id {get_vertex_id(target)};
+
+    assert(s_id.has_value() && t_id.has_value()); //确认端点存在
+
+    auto& edge_list{_adjacency_list[s_id.value()]}; //获得起点的出边表
+    edge_list.push_back({.to=t_id.value(), .cost=cost}); //插入新的边
+}
+
+template <typename T>
+auto hbutds::Graph<T>::get_edge_cost_by_id(
+        const unsigned int s_id, const unsigned int t_id
+) const -> double {
+    auto& edge_list{_adjacency_list[s_id]};
+    for(auto& e : edge_list){
+        if(e.to == t_id) return e.cost;
+    }
+    return std::numeric_limits<double>::infinity();
+}
+
+template <typename T>
+auto hbutds::Graph<T>::get_edge_cost(
+        const T& source, const T& target
+) const -> double{
+    auto s_id {get_vertex_id(source)};
+    auto t_id {get_vertex_id(target)};
+
+    assert(s_id.has_value() && t_id.has_value()); //确认端点存在
+    return get_edge_cost_by_id(s_id.value(), t_id.value());
+}
+
+#endif
