b4a71787f269f4c362be12c78b177ebf698d57ee
xavierwoo <xavier.woo@gmail.com>
2025-08-20 22:25:40 +0800

    get_path_from_tree函数的tree参数加const

--- graph.h
+++ graph.h
@@ -73,7 +73,7 @@ namespace hbutds{
         // 从生成树中获得路径
         auto get_path_from_tree(
                 const unsigned int, const unsigned int, 
-                vector<std::optional<unsigned int>>&) const -> vector<T>;
+                const vector<std::optional<unsigned int>>&) const -> vector<T>;
     }; 
 
     template <typename T>
@@ -295,7 +295,7 @@ template <typename T>
 auto hbutds::Graph<T>::get_path_from_tree(
         const unsigned int start_id,
         const unsigned int end_id,
-        vector<std::optional<unsigned int>>& tree
+        const vector<std::optional<unsigned int>>& tree
 ) const -> vector<T>{
     vector<T> path;
     std::optional curr(end_id);
