1c219e9567c0a9a778efdddc3190ec771adbccdd
xavierwoo <xavier.woo@gmail.com>
2025-12-04 11:05:01 +0800

    添加array::iterator::operator+

--- array.h
+++ array.h
@@ -28,6 +28,10 @@ namespace hbutds{
         auto operator++() -> iterator&; // 获取后继元素的迭代器
         auto operator*() -> T&; // 获取迭代器所指元素
         auto operator!=(iterator) const -> bool; //判断与某迭代器是否不等
+
+        auto operator--() -> iterator&; // 获取前序元素的迭代器
+        auto operator+(int) -> iterator; // 通过加法获得相对位置的迭代器
+        auto operator-(int) -> iterator; // 通过减法获得相对位置的迭代器
     };
 
 
@@ -61,6 +65,17 @@ template<typename T, unsigned int N>
 auto hbutds::array<T, N>::iterator::operator++() -> iterator& {
     ++_ptr;
     return *this;
+}
+
+template<typename T, unsigned int N>
+auto hbutds::array<T, N>::iterator::operator--() -> iterator& {
+    --_ptr;
+    return *this;
+}
+
+template<typename T, unsigned int N>
+auto hbutds::array<T, N>::iterator::operator+(int offset) -> iterator {
+    return iterator(_ptr + offset);
 }
 
 template<typename T, unsigned int N>
