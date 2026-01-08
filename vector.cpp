df907d5e67a5ce7283047ea2baf0408762c0c08c
xavierwoo <xavier.woo@gmail.com>
2026-01-08 16:33:13 +0800

    vector简单版测试代码

--- vector.cpp
+++ vector.cpp
@@ -23,10 +23,53 @@ namespace hbutds{
         cout<<"\tInitializer and access works!\n";
     }
 
+    void test_vector_iterator(){
+        vector<int> vec{1,2,3};
+        auto it{vec.begin()};
+        assert(*it == 1);
+        ++it;
+        assert(*it == 2);
+        ++it;
+        assert(*it == 3);
+        ++it;
+        assert(! (it != vec.end()));
+        cout<<"\tIterator works!\n";
+    }
+
+    void test_vector_iterator_calculation(){
+        vector<int> vec{1,2,3};
+        assert(*(vec.begin() + 2) == 3);
+        assert(*(vec.end() - 2) == 2);
+        cout<<"\tIterator calculation works!\n";
+    }
+
+    void test_vector_insert(){
+        vector<int> vec{1,2,3};
+        auto it {vec.insert(vec.begin(), 0)};
+        assert(vec.size() == 4);
+        assert(*it == 0);
+        assert(*(it+1) == 1);
+        vec.push_back(0);
+        assert(vec[4] == 0);
+        cout<<"\tInsert works!\n";
+    }
+
+    void test_vector_erase(){
+        vector<int> vec{1,2,3};
+        auto it {vec.erase(vec.begin())};
+        assert(*it == 2);
+        assert(vec.size() == 2);
+        cout<<"\tErase works!\n";
+    }
+
     void vector_works(){
         cout<<"Testing hbutds::vector simple version:\n";   
         test_vector_reserve();
         test_vector_initializer_and_access();
+        test_vector_iterator();
+        test_vector_iterator_calculation();
+        test_vector_insert();
+        test_vector_erase();
         cout<<"\thbutds::vector works!\n";
     }
 }
